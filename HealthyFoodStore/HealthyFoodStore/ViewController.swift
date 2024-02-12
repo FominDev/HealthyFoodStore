1ad2e3c982326767e618214a5244a27c4055ed9e
FominDev <artfomindev@gmail.com>
2024-02-12 23:27:06 +0300

    Created project, deleted storyboard.

---
+++ HealthyFoodStore/HealthyFoodStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthyFoodStore
+//
+//  Created by Артур Фомин on 12.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .brown
+    }
+
+
+}
+
