3d02918ae81f81c4901480f8af3882074086b90b
FominDev <artfomindev@gmail.com>
2024-02-17 15:40:08 +0300

    Add app icons and custom colors

---
+++ HealthyFoodStore/HealthyFoodStore/Resources/AppColors.swift
@@ -0,0 +1,14 @@
+//
+//  AppColors.swift
+//  HealthyFoodStore
+//
+//  Created by Артур Фомин on 17.02.2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let background = UIColor().hex(0xECF0F1)
+    static let customBlue = UIColor().hex(0x46cbdc)
+    static let customGreen = UIColor().hex(0x45e384)
+}
