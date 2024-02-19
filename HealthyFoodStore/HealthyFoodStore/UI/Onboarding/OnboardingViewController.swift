98ecca61c188bf108e3bdcf0a187b27191515dea
FominDev <artfomindev@gmail.com>
2024-02-19 13:58:21 +0300

    Fix incorrect page control circles

--- HealthyFoodStore/HealthyFoodStore/UI/Onboarding/OnboardingViewController.swift
+++ HealthyFoodStore/HealthyFoodStore/UI/Onboarding/OnboardingViewController.swift
@@ -13,6 +13,7 @@ class OnboardingViewController: UIViewController {
     
     //MARK: - Properties
     private var pages = [OnboardingPageViewController]()
+    private var currentPageIndex = 0
     
     //MARK: - Views
     private let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
@@ -134,8 +135,13 @@ extension OnboardingViewController: UIPageViewControllerDataSource {
 extension OnboardingViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
         if let index = pages.firstIndex(of: pendingViewControllers.first! as! OnboardingPageViewController) {
-            pageControl.currentPage = index
-            let page = pages[index]
+            currentPageIndex = index
+        }
+    }
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            pageControl.currentPage = currentPageIndex
+            let page = pages[currentPageIndex]
             let title = page.buttonText
             finishButton.setTitle(title, for: .normal)
         }
