fa73faeb9ae538e1936818618b2ccee565e75969
FominDev <artfomindev@gmail.com>
2024-02-25 16:20:58 +0300

    Created Cart view controller and table view cell files

---
+++ HealthyFoodStore/HealthyFoodStore/UI/Cart/CartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CartTableViewCell.swift
+//  HealthyFoodStore
+//
+//  Created by Артур Фомин on 25.02.2024.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HealthyFoodStore/HealthyFoodStore/UI/Cart/CartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CartViewController.swift
+//  HealthyFoodStore
+//
+//  Created by Артур Фомин on 25.02.2024.
+//
+
+import UIKit
+
+class CartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
